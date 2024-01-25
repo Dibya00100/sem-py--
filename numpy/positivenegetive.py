e93b88aaa4432c163d3ce766c1e8d8211722deb5
Dibya00100 <86556595+Dibya00100@users.noreply.github.com>
2024-01-25 11:49:04 +0530

    semester 3

---
+++ numpy/positivenegetive.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number: "))
+
+if n > 0:
+    print("Number is positve.")
+elif n < 0:
+    print("Number is negative.")
+else:
+    print("the number is 0")
---
+++ numpy/vowel.py
@@ -0,0 +1,34 @@
+# char = input("Enter a character: ")
+# vowel = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+#
+# # for n in vowel:
+# if char == vowel:
+# print("Vowel")
+#
+# else:
+#         print("consonant")
+
+char = input("enter a character: ")
+
+if char == "a":
+    print("vowel")
+elif char == 'e':
+    print("vowel")
+elif char == 'i':
+    print("vowel")
+elif char == 'o':
+    print("vowel")
+elif char == 'u':
+    print("vowel")
+elif char == 'A':
+    print("vowel")
+elif char == 'E':
+    print("vowel")
+elif char == 'I':
+    print("vowel")
+elif char == 'O':
+    print("vowel")
+elif char == 'U':
+    print("vowel")
+else:
+    print("Consonant")
